f683d2d397b690be4a144ab8c62c7a087600f39c
noeTem <noel.temores@sjsu.edu>
2026-03-22 00:33:57 -0700

    python script to test serial commands to roomba; testing 7-segment display

---
+++ src/scripts/led_test.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+ser = serial.Serial('COM3', 115200, timeout=1) # adjust COM port as needed
+time.sleep(2)
+
+# Star open interface
+ser.write(bytes([128])) # 128 start code
+time.sleep(0.1)
+
+# Safe mode
+ser.write(bytes([131]))
+time.sleep(0.1)
+
+# Display "LUCI"
+ser.write(bytes([164, 76, 85, 67, 73]))
+
+time.sleep(5)
+
+ser.close()
